b0232a2a5d821da629d2a9c603d7e2e65ab1db8a
C-quark <90129611+C-quark@users.noreply.github.com>
2024-02-12 02:46:43 +0300

    выгрузка таблиц импорта

--- main.py
+++ main.py
@@ -82,23 +82,88 @@ def analyzer(file):
             align_rva = alignment * (rva // alignment) + alignment
             return align_rva
 
-        point_section_header_start = f.seek(point_optional_header_start + optional_header_size)
+        f.seek(point_optional_header_start + optional_header_size)
         section_header_size = BYTE_8 + BYTE_4 * 7 + BYTE_2 * 2
         for i in range(file_header_number_of_sections_value):
             section_header = f.read(section_header_size)
             section_header_virtual_size_value = struct.unpack('<I', section_header[BYTE_8:BYTE_8+BYTE_4])[0]
             section_header_va_value = struct.unpack('<I', section_header[BYTE_8+BYTE_4:BYTE_8+BYTE_4*2])[0]
             section_header_pointer_to_raw_data_value = struct.unpack('<I', section_header[BYTE_8+BYTE_4*3:BYTE_8+BYTE_4*4])[0]
-
             start_section = section_header_va_value
             end_section = start_section + align(section_header_virtual_size_value, section_alignment_value)
-            if import_directory_va_value >= start_section and import_directory_va_value < end_section:
-                print(i, section_header_virtual_size_value, section_header_pointer_to_raw_data_value)
+            if start_section <= import_directory_va_value < end_section:
+                print(i, section_header_va_value, section_header_pointer_to_raw_data_value)
                 break
 
-        import_directory_table = import_directory_va_value - section_header_virtual_size_value + section_header_pointer_to_raw_data_value
+        import_directory_table = import_directory_va_value - section_header_va_value + section_header_pointer_to_raw_data_value
         print(import_directory_table)
+
+        f.seek(import_directory_table)
+        import_descriptor_size = 5 * BYTE_4
+
+        original_first_thunk_list = []
+        name_list = []
+        first_thunk_list = []
+        for i in range(import_descriptor_size):
+            import_descriptor = f.read(import_descriptor_size)
+            import_descriptor_original_first_thunk = struct.unpack('<I', import_descriptor[0:BYTE_4])[0]
+            original_first_thunk_list.append(import_descriptor_original_first_thunk)
+            import_descriptor_name = struct.unpack('<I', import_descriptor[BYTE_4*3:BYTE_4*4])[0]
+            name_list.append(import_descriptor_name)
+            import_descriptor_first_thunk = struct.unpack('<I', import_descriptor[BYTE_4*4:BYTE_4*5])[0]
+            first_thunk_list.append(import_descriptor_first_thunk)
+
+            if import_descriptor_original_first_thunk == 0:
+                original_first_thunk_list.remove(import_descriptor_original_first_thunk)
+                name_list.remove(import_descriptor_name)
+                first_thunk_list.remove(import_descriptor_first_thunk)
+                break
+
+        print(original_first_thunk_list)
+        print(name_list)
+        print(first_thunk_list)
+
+        for i in name_list:
+            import_descriptor_name_raw = i - section_header_va_value + section_header_pointer_to_raw_data_value
+            f.seek(import_descriptor_name_raw)
+            while True:
+                name = f.read(BYTE)
+                name_value = struct.unpack('<B', name)[0]
+                #print(name)
+                if name_value == 0:
+                    break
+
+        thunk_data_list = []
+        for i in original_first_thunk_list:
+            original_first_thunk_raw = i - section_header_va_value + section_header_pointer_to_raw_data_value
+            f.seek(original_first_thunk_raw)
+            while True:
+                if optional_header_magic_value == 267:
+                    thunk_data_size = BYTE_4
+                    thunk_data = f.read(thunk_data_size)
+                    thunk_data_value = struct.unpack('<I', thunk_data)[0]
+                    thunk_data_list.append(thunk_data_value)
+                elif optional_header_magic_value == 523:
+                    thunk_data_size = BYTE_8
+                    thunk_data = f.read(thunk_data_size)
+                    thunk_data_value = struct.unpack('<Q', thunk_data)[0]
+                    thunk_data_list.append(thunk_data_value)
+                elif thunk_data_value == 0:
+                    break
+
+        print(thunk_data_list)
+
+        for i in thunk_data_list:
+            thunk_data_raw = i - section_header_va_value + section_header_pointer_to_raw_data_value
+            f.seek(thunk_data_raw)
+            while True:
+                import_by_name_size = BYTE_4 + BYTE
+                import_by_name = f.read(import_by_name_size)
+                import_by_name_value = struct.unpack('<B', import_by_name[BYTE_4:BYTE_4+BYTE])[0]
+                print(import_by_name)
+                if import_by_name_value == 0:
+                    break
 
 
 if __name__ == "__main__":
-    analyzer('notepad++.exe')
+    analyzer('win_app.exe')
